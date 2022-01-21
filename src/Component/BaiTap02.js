fbcb10c91b56626ad364c4ae91c00c79e8bfbf1c
Quy Huynh <huynhquy9x@gmail.com>
2022-01-21 16:03:23 +0700

    update css to styles

--- src/Component/BaiTap02.js
+++ src/Component/BaiTap02.js
@@ -4,27 +4,17 @@ import {Text, View, StyleSheet} from 'react-native';
 export default class BaiTap02 extends Component{
     render() {
         return (
-            <View style={{flex: 1}}>
-                <View style={{flex: 1, backgroundColor: '#A1c99A',
-                    alignItems: "center",
-                    borderBottomEndRadius: 10,
-                    borderBottomStartRadius: 10}}>
-                    <View style={{backgroundColor: '#ffffc2', margin: 12, marginTop: 40, width: '90%', height: 100, borderRadius: 10}}></View>
-                    <View style={{backgroundColor: '#ffffc2', margin: 12, marginTop: 40, width: '90%', height: 100, borderRadius: 10}}></View>
+            <View style={styles.container}>
+                <View style={styles.top}>
+                    <View style={styles.box1}></View>
+                    <View style={styles.box1}></View>
                 </View>
                 
-                <View style={{flex: 1, marginTop: 10,
-                     backgroundColor: '#c8c8fA',
-                     flexDirection: 'row', 
-                     alignItems: 'center',
-                     flexWrap: 'wrap',
-                     justifyContent: 'center',
-                     borderTopStartRadius: 10, 
-                     borderTopEndRadius: 10}}>
-                    <View style={{backgroundColor: '#4f82c0', margin: 12, width: 140, height: 140, borderRadius: 10}}></View>
-                    <View style={{backgroundColor: '#4f82c0', margin: 12, width: 140, height: 140, borderRadius: 10}}></View>
-                    <View style={{backgroundColor: '#4f82c0', margin: 12, width: 140, height: 140, borderRadius: 10}}></View>
-                    <View style={{backgroundColor: '#4f82c0', margin: 12, width: 140, height: 140, borderRadius: 10}}></View>
+                <View style={styles.bottom}>
+                    <View style={styles.box2}></View>
+                    <View style={styles.box2}></View>
+                    <View style={styles.box2}></View>
+                    <View style={styles.box2}></View>
                 </View>
             </View>
         );
@@ -35,28 +25,36 @@ const styles = StyleSheet.create({
     container: {
         flex: 1
     },
-    box: {
+    top: {
         flex: 1,
-        borderRadius: 10,
+        backgroundColor: '#A1c99A',
+        alignItems: "center",
+        borderBottomEndRadius: 10,
+        borderBottomStartRadius: 10
+    },
+    bottom: {
+        flex: 1, marginTop: 10,
+        backgroundColor: '#c8c8fA',
+        flexDirection: 'row', 
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        borderTopStartRadius: 10, 
+        borderTopEndRadius: 10
     },
     box1: {
-        width: 300,
-        height: 10,
+        backgroundColor: '#ffffc2', 
+        margin: 12, 
+        marginTop: 40,
+         width: '90%', 
+         height: 100,
+          borderRadius: 10
     },
-    purple: {
-        backgroundColor: '#C9C9FF'
-    },
-    blue: {
-        backgroundColor: '#3D85C6'
-    },
-    green: {
-        backgroundColor: '#A1c99A'
-    },
-    yellow: {
-        backgroundColor: '#ffffc2'
-    },
-    infoTxt: {
-        fontSize: 20,
-        fontWeight: 'bold'
+    box2: {
+        backgroundColor: '#4f82c0',
+        margin: 12, 
+        width: 140, 
+        height: 140, 
+        borderRadius: 10
     }
 });
